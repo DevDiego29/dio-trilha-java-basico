a4494a30e521e7063f8382d3ce565eab94d51140
Diego <adv.diego21@gmail.com>
2023-08-31 08:48:53 -0300

    tipos primitivos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,34 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Tipos primitivos e variáveis:
+
+        //Texto
+
+        String nomeCompleto = "Diego Rodrigues Alves";
+
+        //Números Inteiros
+
+        byte idade = 8;
+        short ano = 2023;
+        int cep = 58039265;
+        long cpf = 10140547689L;
+
+        //Números reais 
+
+        float pi = 3.14F;
+        double salarioMinimo = 2500.33;
+
+        //casting
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        
+        //Palavra reservada
+
+        final int numero = 1;
+        System.out.print(numero);
+        
     }
 }
