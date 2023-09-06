c77e7f567ec537a5036638b08f8e67705eb47cde
Diego <adv.diego21@gmail.com>
2023-09-06 08:11:11 -0300

    Estruturas condicionais

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 7;
+
+        if (nota >= 7) {
+            System.out.println("Aluno aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Em recuperação");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
